cf931a0aa783d64742584b85a9d6fafdb04b022e
anadolacio <anadolacio@hotmail.com>
2023-04-27 15:55:04 -0300

    requisito 12, reoslvendo erros do lint

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -22,7 +22,7 @@ const deleteProduct = async (req, res) => {
   const { id } = req.params;
   const numberId = Number(id);
   const product = await productsService.getOnlyIdProducts(numberId);
-  console.log(product);
+  // console.log(product);
   if (!product) return res.status(404).json({ message: 'Product not found' });
   await productsService.deleteProduct(numberId);
   return res.status(204).json();
